dd190f6eedc31b6c7064ed8cf9c2b3d4de383c2d
lahotikunal <37633212+lahotikunal@users.noreply.github.com>
2021-05-08 19:05:54 +0530

    Update lucky7jv45.js

--- lucky7jv45.js
+++ lucky7jv45.js
@@ -14,7 +14,7 @@ function myRadioValue(dice) {
 //slider code
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value;
+
 
 slider.oninput = function() {
   output.innerHTML = this.value;
